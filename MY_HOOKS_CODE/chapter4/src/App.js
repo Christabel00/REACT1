942215a0baa70ed745bfbefb10e84cc3c8d84c36
Christabel00 <ruvimbochristabel@gmail.com>
2024-10-02 09:30:25 +0200

    Initial commit

---
+++ MY_HOOKS_CODE/chapter4/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Products from './Products'
+import Rating from './Rating'
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName
+}
+
+function App() {
+  const isValid = true
+
+  return (
+    <div>
+      <Rating rating='1' />
+      <Rating rating='2' />
+      <Rating rating='3' />
+      <Rating rating='4' />
+      <Rating rating='5' />
+      {/* <Products />   
+      <Button  variant="danger">Default</Button>   */}
+    </div>
+  )
+}
+
+export default App
---
+++ MY_HOOKS_CODE/chapter7/src/GitHub.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'; // npm install axios 
+import Card from 'react-bootstrap/Card';
+function GitHub() {
+    const [data, setData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        axios.get("https://api.github.com/search/users?q=greg")
+            .then(res => {
+                // console.log(res.data.items);
+                setData(res.data.items)
+                setIsLoading(false);
+            });
+    }, [])
+    const listUsers = data.map((user) =>              
+        <Card key={user.id}> 
+            <a href={user.html_url}>     
+                <img 
+                    width={64} 
+                    height={64} 
+                    className="mr-3" 
+                    src={user.avatar_url} 
+                    alt="Generic placeholder" 
+                /> 
+            </a> 
+            <Card.Body> 
+                <h5>Login: {user.login}</h5> 
+                <p>Id: { user.id }</p> 
+            </Card.Body> 
+        </Card>   
+    ); 
+    return (
+        <div>
+             <h3>GitHub Users Results</h3> 
+            {isLoading &&
+              < ReactLoading type="spinningBubbles" color="#444" /> 
+            }
+             {listUsers} 
+        </div>
+    );
+}
+export default GitHub; 
